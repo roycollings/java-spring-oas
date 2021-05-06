1fe258f2fe5f55e16986d92bbd102f4478a56c91
Roy Collings <roy_collings@discovery.com>
2021-05-06 16:21:44 +0200

    nicer use of lambdas

--- src/main/java/com/example/springbootswagger2/controller/Students.java
+++ src/main/java/com/example/springbootswagger2/controller/Students.java
@@ -1,14 +1,10 @@
 package com.example.springbootswagger2.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.webjars.NotFoundException;
 
@@ -46,15 +42,11 @@ public class Students {
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Found the student", content = {
           @Content(mediaType = "application/json", schema = @Schema(implementation = Student.class)) }),
-      @ApiResponse(responseCode = "500", description = "Failed to find a student", content = @Content) })
+      @ApiResponse(responseCode = "500", description = "Student not found", content = @Content) })
   @GetMapping(value = "/student/{name}")
   public Student getStudent(
       @Parameter(description = "Name of stdudent. Cannot be empty.", required = true, example = "Sajal") @PathVariable String name) {
-    Optional<Student> foundStudent = students.stream().filter(student -> student.getName().equals(name)).findFirst();
-    if (foundStudent == null) {
-      throw new NotFoundException("Student not found");
-    }
-
-    return foundStudent.get();
+    return students.stream().filter(student -> student.getName().equals(name)).findFirst()
+        .orElseThrow(() -> new NotFoundException(">>> MSG 1"));
   }
 }
