239e6c799ad8ac898dc2e3d882ff84a230bebbf6
Roy Collings <roy_collings@discovery.com>
2021-05-06 12:46:27 +0200

    Can see swagger json at http://localhost:8080/swagger.json

--- src/main/java/com/example/springbootswagger2/DemoApplication.java
+++ src/main/java/com/example/springbootswagger2/DemoApplication.java
@@ -2,12 +2,31 @@ package com.example.springbootswagger2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import com.example.springbootswagger2.controller.Swagger2DemoRestController;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackageClasses = { Swagger2DemoRestController.class })
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
 	}
 
 }
--- src/main/java/com/example/springbootswagger2/controller/Swagger2DemoRestController.java
+++ src/main/java/com/example/springbootswagger2/controller/Swagger2DemoRestController.java
@@ -2,8 +2,6 @@ package com.example.springbootswagger2.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.springbootswagger2.model.Student;
@@ -15,31 +13,10 @@ public class Swagger2DemoRestController {
   {
     students.add(new Student("Sajal", "IV", "India"));
     students.add(new Student("Lokesh", "V", "India"));
-    students.add(new Student("Kajal", "III", "USA"));
-    students.add(new Student("Sukesh", "VI", "USA"));
   }
 
   @RequestMapping(value = "/getStudents")
   public List<Student> getStudents() {
     return students;
-  }
-
-  @RequestMapping(value = "/getStudent/{name}")
-  public Student getStudent(@PathVariable(value = "name") String name) {
-    return students.stream().filter(x -> x.getName().equalsIgnoreCase(name)).collect(Collectors.toList()).get(0);
-  }
-
-  @RequestMapping(value = "/getStudentByCountry/{country}")
-  public List<Student> getStudentByCountry(@PathVariable(value = "country") String country) {
-    System.out.println("Searching Student in country : " + country);
-    List<Student> studentsByCountry = students.stream().filter(x -> x.getCountry().equalsIgnoreCase(country))
-        .collect(Collectors.toList());
-    System.out.println(studentsByCountry);
-    return studentsByCountry;
-  }
-
-  @RequestMapping(value = "/getStudentByClass/{cls}")
-  public List<Student> getStudentByClass(@PathVariable(value = "cls") String cls) {
-    return students.stream().filter(x -> x.getCls().equalsIgnoreCase(cls)).collect(Collectors.toList());
   }
 }
