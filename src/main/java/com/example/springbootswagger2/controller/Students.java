5c3b313a89ab05eda5bba188302cd60412310c2d
Roy Collings <roy_collings@discovery.com>
2021-05-06 13:20:21 +0200

    Annotations on the route

--- src/main/java/com/example/springbootswagger2/controller/Students.java
+++ src/main/java/com/example/springbootswagger2/controller/Students.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import com.example.springbootswagger2.model.Student;
 
 @RestController
@@ -17,6 +23,11 @@ public class Students {
     students.add(new Student("Lokesh", "V", "India"));
   }
 
+  @Operation(summary = "Get a list of all students")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Found the students", content = {
+          @Content(mediaType = "application/json", schema = @Schema(implementation = Student.class)) }),
+      @ApiResponse(responseCode = "500", description = "Failed to find students", content = @Content) })
   @RequestMapping(value = "/students", method = RequestMethod.GET)
   public List<Student> getStudents() {
     return students;
